59217e7423548eaedb4692280e77275e4cf63077
SleepyGom <93717960+SleepyGom@users.noreply.github.com>
2023-06-26 13:12:12 +0900

    making pizza worm game with js

---
+++ pizzaGame/pizza.js
@@ -0,0 +1,16 @@
+class Pizza{
+    constructor() {
+        this.pos = new Tile(10, 20);
+
+    }
+    renderPizza(){
+        this.pos.renderImg();
+    }
+
+    randomPizza() {
+        const col = Math.floor(Math.random()) * (tileWidth - 2) + 1;
+        const row = Math.floor(Math.random()) * (tileWidth - 2) + 1;
+        this.pos = new Tile(col,row)
+        
+    }
+}
---
+++ pizzaGame/tile.js
@@ -0,0 +1,26 @@
+class Tile {
+    constructor(col, row) {
+        this.col = col;
+        this.row = row;
+        this.posX = this.col * tileSize;
+        this.posY = this.row * tileSize;
+    }
+
+    renderTile(bg = 'green') {
+        ctx.fillStyle = bg;
+        ctx.fillRect(this.posX , this.posY, tileSize, tileSize);
+    }
+
+    renderImg(bg = './img/pizza.png') {
+        const image = new Image(tileSize, tileSize);
+        image.src = bg;
+        image.addEventListener('load', () => {
+            ctxBg.drawImage(image, this.posX, this.posY, tileSize, tileSize)
+        })
+    }
+
+    // 충돌을 체크 하는 함수
+    collisionCheck(target) {
+        return this.col === target.col && this.row === target.row
+    }
+}
